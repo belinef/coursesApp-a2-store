d8f46bae8dca7e085e243aab0d3bfd365332d589
Ievgenii_Litvinov <ievgenii_litvinov@epam.com>
2017-08-17 17:20:25 +0300

    some changes

--- src/app/common/profile/profile.effects.ts
+++ src/app/common/profile/profile.effects.ts
@@ -5,28 +5,22 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toArray';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
 import { Actions,
   Effect,
   toPayload
 } from '@ngrx/effects';
-import { Observable } from 'rxjs/Observable';
 import { ActionTypes,
   FetchedProfile,
   ConfirmAvatarImage,
   NameConfirmed,
   BirthDateConfirmed } from './profile.actions';
-import { Router } from '@angular/router';
 
 import { ProfileService } from '../../core/services/profile/profile.service';
 
 @Injectable()
 export class ProfileEffects {
-  private url: string = '/api/profile';  // URL to web API
   constructor(
-    private http: Http,
     private actions$: Actions,
-    private router : Router,
     private profileService: ProfileService
   ) {}
 
@@ -50,16 +44,17 @@ export class ProfileEffects {
     .switchMap((avatar) => {
       const result = Object.assign({}, avatar, { original: avatar.original.src});
 
-      return this.profileService.updateProfile('avatar', result).map(() => new ConfirmAvatarImage(result))
-            ;});
+      return this.profileService.updateProfile('avatar', result)
+        .map(() => new ConfirmAvatarImage(result));
+  });
 
   @Effect()
   nameUpdating$ = this.actions$
     .ofType(ActionTypes.FETCH_NAME_UPDATE)
     .map(toPayload)
     .switchMap((name) => {
-      return this.profileService.updateProfile('name', name).map(() => new NameConfirmed(name))
-        ;});
+      return this.profileService.updateProfile('name', name).map(() => new NameConfirmed(name));
+  });
 
   @Effect()
   dateofBirthUpdating$ = this.actions$
@@ -67,6 +62,7 @@ export class ProfileEffects {
     .map(toPayload)
     .switchMap(({ data: {formatted} }) => {
 
-      return this.profileService.updateProfile('dateOfBirth', formatted).map(() => new BirthDateConfirmed())
-        ;});
+      return this.profileService.updateProfile('dateOfBirth', formatted)
+        .map(() => new BirthDateConfirmed());
+  });
 }
